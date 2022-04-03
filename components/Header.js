2eaa82a519972c8da3a82f3a0f165fbd4148335d
gioadorno <adorno.gio@gmail.com>
2022-04-02 19:54:49 -0700

    day one build

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className='sticky-header top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
         <div className='relative flex items-center h-10 my-auto'>
-            <Image src='/TheStay.svg' layout="fill" objectFit="contain" objectPosition='left' />
+            <Image alt='image' src='/TheStay.svg' layout="fill" objectFit="contain" objectPosition='left' />
         </div>
         <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
             <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" placeholder="Start your search" />
--- components/SmallCard.js
+++ components/SmallCard.js
@@ -4,7 +4,7 @@ const SmallCard = ({ img, location, distance }) => {
   return (
     <div className='flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-50 hover:scale-105 transition transform duration-200 ease-out'>
         <div className='relative h-16 w-16'>
-            <Image src={img} layout='fill' className='rounded-lg'  />
+            <Image alt='image' src={img} layout='fill' className='rounded-lg'  />
         </div>
         <div>
             <h2>{location}</h2>
