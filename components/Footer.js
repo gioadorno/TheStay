6ecb2871aa8ad41f6809fe3e5c2e809642e742eb
gioadorno <adorno.gio@gmail.com>
2022-04-02 19:52:17 -0700

    day one build

---
+++ components/Footer.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
+        <div className='space-y-4 text-xs text-gray-800'>
+            <h5 className='font-bold'>ABOUT</h5>
+            <p>How TheStay works</p>
+            <p>Newsroom</p>
+            <p>Investors</p>
+            <p>TheStay Plux</p>
+            <p>TheStay Luxe</p>
+        </div>
+        <div className='space-y-4 text-xs text-gray-800'>
+            <h5 className='font-bold'>COMMUNITY</h5>
+            <p>Accessibility</p>
+            <p>This is a showcase site</p>
+            <p>Very awesome</p>
+            <p>Love it</p>
+            <p>Luxury</p>
+        </div>
+        <div className='space-y-4 text-xs text-gray-800'>
+            <h5 className='font-bold'>HOST</h5>
+            <p>TheStay is awesome</p>
+            <p>Find a place</p>
+            <p>Hosting starts here</p>
+            <p>Hosting ends here</p>
+            <p>Join now</p>
+        </div>
+        <div className='space-y-4 text-xs text-gray-800'>
+            <h5 className='font-bold'>SUPPORT</h5>
+            <p>Help Center</p>
+            <p>Trust & Safety</p>
+            <p>Contact us</p>
+            <p>FAQ's</p>
+            <p>We're here to help</p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
